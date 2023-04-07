66e3c3465a2811b0dd4e81764883fca63e847ab6
ardianns <as58581@ubt-uni.net>
2023-04-07 17:37:49 +0200

    add login and signup form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import LogInForm from './Pages/LogInForm';
+import SignUpForm from './Pages/SignUpForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <Router>
+    <Routes>
+        <Route path="/signin" element={<LogInForm />} />
+        <Route exact path="/signup" element={<SignUpForm />} />
+      </Routes>
+    </Router>
+    </>
+  
 }
 
 export default App;
