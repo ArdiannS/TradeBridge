b663fc8f4351b9f28a3f526e4fbe0d21b6f1ef06
albin-sh <albinsh53@gmail.com>
2023-04-09 16:16:36 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
-import LogInForm from './Components/LogInForm';
-import SignUpForm from './Components/SignUpForm';
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
-
+import "./App.css";
+import LogInForm from "./components/LogInForm";
+import SignUpForm from "./components/SignUpForm";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <>
-
+  return (
+    <>
       <Router>
-      <Navbar/>
-      <img src="https://picsum.photos/id/237/1920/1080" />
-        <Routes>
-        {/* <Route exact path="/" element={< Home/>} /> */}
-        <Route path="/signin" element={<LogInForm />} />
-        <Route exact path="/signup" element={<SignUpForm />} />
-      </Routes>
-    </Router>
-    <div>
-    <Footer />
+        <div>
+          <Navbar />
+        </div>
 
-    </div>
+        <Routes>
+          {/* <Route exact path="/" element={< Home/>} /> */}
+          <Route path="/signin" element={<LogInForm />} />
+          <Route exact path="/signup" element={<SignUpForm />} />
+        </Routes>
+      </Router>
+      <div>
+        <Footer />
+      </div>
     </>
+  );
 }
 
 export default App;
