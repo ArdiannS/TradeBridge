614c4fac8ac8f23edf1fa6d7563bcc4dc24e0fb0
argjend02 <argjendazizi36@gmail.com>
2023-04-09 16:09:43 +0200

    add main page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import './App.css';
-import LogInForm from './Components/LogInForm';
-import SignUpForm from './Components/SignUpForm';
+import "./App.css";
+import LogInForm from "./Components/LogInForm";
+import SignUpForm from "./Components/SignUpForm";
 import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
-
+import Navbar from "./Components/navbar";
+import Home from "./Components/Home";
+import Support from "./Components/Support";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <>
-
+  return (
+    <>
       <Router>
-      <Navbar/>
-      <img src="https://picsum.photos/id/237/1920/1080" />
-        <Routes>
-        {/* <Route exact path="/" element={< Home/>} /> */}
-        <Route path="/signin" element={<LogInForm />} />
-        <Route exact path="/signup" element={<SignUpForm />} />
-      </Routes>
-    </Router>
-    <div>
-    <Footer />
+        <header>
+          <Navbar />
+        </header>
 
-    </div>
+        <div></div>
+        <div>
+          <Home />
+          <Support />
+        </div>
+        <Routes>
+          {/* <Route exact path="/" element={< Home/>} /> */}
+          <Route path="/signin" element={<LogInForm />} />
+          <Route exact path="/signup" element={<SignUpForm />} />
+        </Routes>
+      </Router>
+      <div>
+        <Footer />
+      </div>
     </>
+  );
 }
 
 export default App;
