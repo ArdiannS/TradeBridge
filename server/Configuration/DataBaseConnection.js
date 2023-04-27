fc206026e48c5bff16915dcf3ddb7316c30e3c86
ArdiannS <ardiannsutaj@gmail.com>
2023-04-28 01:52:20 +0200

    add the delete,read for user

---
+++ server/Configuration/DataBaseConnection.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "Arbs",
+    password: "root",
+    database: "TradeBridgeDb",
+  });
+  conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connection succesful...");
+  });
+  module.exports = conn;
--- src/App.js
+++ src/App.js
@@ -9,27 +9,28 @@ import PostJobs from "./Pages/PostJobs";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PreLoader from "./Pages/PreLoader";
 import React, { useState, useEffect } from "react";
+import Dashboard from "./Pages/DashBoard";
 function App() {
-  const[backendData,setBackEndData] = useState([{}]);
-  useEffect(()=>{
-    fetch("/api").then(
-    response => response.json()
-    ).then(
-      data=>{
-        setBackEndData(data)
-      }
-    )
-},[])
-const[userdata,setUserData] = useState([]);
-useEffect(()=>{
-  fetch("/api/users").then(
-    response => response.json()
-    ).then(
-      data=>{
-        setBackEndData(data)
-      }
-    )
-},[])
+//   const[backendData,setBackEndData] = useState([{}]);
+//   useEffect(()=>{
+//     fetch("/api").then(
+//     response => response.json()
+//     ).then(
+//       data=>{
+//         setBackEndData(data)
+//       }
+//     )
+// },[])
+// const[userdata,setUserData] = useState([]);
+// useEffect(()=>{
+//   fetch("/api/users").then(
+//     response => response.json()
+//     ).then(
+//       data=>{
+//         setBackEndData(data)
+//       }
+//     )
+// },[])
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -57,6 +58,8 @@ useEffect(()=>{
           <Route path="/" exact element={<Home />} />
           <Route path="/signin" exact element={<LogInForm />} />
           <Route path="/signup" exact element={<SignUpForm />} />
+          <Route path="/dashboard" exact element={<Dashboard />} />
+
           <Route path="/postjobs" exact element={<PostJobs />} />
           <Route path="/jobsearch" exact element={<JobSearch />} />
         </Routes>
