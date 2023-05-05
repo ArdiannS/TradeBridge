c7ea0428c3bdb41c0c5525cca5c9f4ced7eed56a
argjend02 <argjendazizi36@gmail.com>
2023-05-05 02:40:07 +0200

    added dashboard changes

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Stats from "../Components/Stats";
 import Sidebar from "../Components/Sidebar";
-import { Link } from "react-router-dom";
+import { Link }  from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "../api/axiosInstance";
 
