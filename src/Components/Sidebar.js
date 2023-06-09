4067bd23437e3ed1322b16bee90b01052bbd783c
albin-sh <albinsh53@gmail.com>
2023-06-09 22:36:42 +0200

    add chart to Dashboard and made style modifications to it

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,94 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartBar,
+  faUsers,
+  faBriefcase,
+  faComments,
+} from "@fortawesome/free-solid-svg-icons";
+
 const user = JSON.parse(localStorage.getItem("user"));
 console.log("user", user);
 
 function Sidebar() {
   return (
-    <div>
-      <div class="flex flex-col w-56 bg-white shadow-lg h-full">
-        <div class="flex items-center justify-center h-16 text-lg font-bold text-indigo-500 bg-white">
-          <span>Dashboard</span>
-        </div>
-        <ul class="flex flex-col py-4">
-          <li>
-            <Link
-              to="/user/profile"
-              href="#"
-              class="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i class="bx bx-music mr-3"></i>
-              <span>User Profile</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={`/myjobs/${user.userid}`}
-              className="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i className="bx bx-music mr-3"></i>
-              <span>My jobs</span>
-            </Link>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i class="bx bx-drink mr-3"></i>
-              <span>Comments</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i class="bx bx-shopping-bag mr-3"></i>
-              <span>Shopping</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i class="bx bx-chat mr-3"></i>
-              <span>Chat</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i class="bx bx-user mr-3"></i>
-              <span>Profile</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i class="bx bx-bell mr-3"></i>
-              <span>Notifications</span>
-              <span class="ml-auto text-xs font-semibold text-red-500 bg-red-100 rounded-full px-3 py-1">
-                5
-              </span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center px-6 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-            >
-              <i class="bx bx-log-out mr-3"></i>
-              <span>Logout</span>
-            </a>
-          </li>
-        </ul>
+    <div className="flex flex-col w-72 bg-gray-100 shadow-lg h-full">
+      <div className="flex items-center justify-center h-16 text-3xl font-bold text-indigo-500 bg-white">
+        <FontAwesomeIcon icon={faChartBar} className="text-4xl mr-2" />
+        <span>Dashboard</span>
       </div>
+      <ul className="flex flex-col py-6 space-y-4">
+        <li>
+          <Link
+            to="/allusers"
+            className="flex items-center px-6 py-3 text-lg text-gray-600 hover:text-indigo-500 hover:bg-gray-200"
+          >
+            <FontAwesomeIcon icon={faUsers} className="text-lg mr-3" />
+            <span>All Users</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/alljobs"
+            className="flex items-center px-6 py-3 text-lg text-gray-600 hover:text-indigo-500 hover:bg-gray-200"
+          >
+            <FontAwesomeIcon icon={faBriefcase} className="text-lg mr-3" />
+            <span>All Jobs</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/allcomments"
+            className="flex items-center px-6 py-3 text-lg text-gray-600 hover:text-indigo-500 hover:bg-gray-200"
+          >
+            <FontAwesomeIcon icon={faComments} className="text-lg mr-3" />
+            <span>Comments</span>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
